907111bca925634a4d19e7c4591b57d03a0d9bfb
hyunsu-ko <hyunsuko@amazon.com>
2024-08-24 03:58:19 +0900

    init: Mimic Lambda Slack App

--- src/listeners/events.py
+++ src/listeners/events.py
@@ -1,12 +1,12 @@
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-from listeners.handlers import meow
+from listeners.handlers import mimic
 
 def listener(app):
     app.event("app_mention")(
         ack=respond_within_3_seconds,  
-        lazy=[meow.handler]  
+        lazy=[mimic.handler]  
     )
 
 def respond_within_3_seconds(ack):
