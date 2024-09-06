db450c827a0f5c9990dd31b7f1eae513eb3f0fd6
iamhansko <iamhansko@kakao.com>
2024-09-07 00:14:24 +0900

    feat: Flag Quiz

--- src/listeners/handlers/game.py
+++ src/listeners/handlers/game.py
@@ -2,12 +2,20 @@ import random
 import csv
 
 def handler(say):
-  f = open("data/movies.csv", "r", encoding="utf-8")
+  f = open("data/countries.csv", "r", encoding="utf-8-sig")
   csv_reader = csv.DictReader(f)
-  movies = list(csv_reader)
-  random_movie = random.choice(movies)
+  countries = list(csv_reader)
+  random_country = random.choice(countries)
 
-  quiz = say(f"문제 : {random_movie["quiz"]}")
-  say(f"정답 : {random_movie["title"]}", thread_ts=quiz["ts"])
+  quiz = say(
+    blocks=[
+      {
+        "type": "image",
+        "image_url": f"https://awskorea-flag-images.s3.ap-northeast-2.amazonaws.com/{random_country['code']}.gif",
+        "alt_text": f"{random_country['code']}"
+      }
+    ]
+  )
+  say(f"정답 : {random_country['korean']}({random_country['english']})", thread_ts=quiz["ts"])
   
   f.close()
